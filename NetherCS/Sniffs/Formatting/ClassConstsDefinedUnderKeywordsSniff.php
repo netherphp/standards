b4c17a0bbbcf6669ad6db242d2a9b164743fc919
bob <bob@cosplaycon.com>
2021-08-24 18:47:24 +0000

    typed properties arent consts

--- NetherCS/Sniffs/Formatting/ClassConstsDefinedUnderKeywordsSniff.php
+++ NetherCS/Sniffs/Formatting/ClassConstsDefinedUnderKeywordsSniff.php
@@ -12,18 +12,28 @@ extends NetherCS\Sniffers\ScopeClassConsts {
 
 	public function
 	Execute():
-	Void {
+	void {
 
 		$StackPtr = $this->StackPtr;
 		$Indent = $this->GetCurrentIndent();
 		$Before = NULL;
-		$ArrayPtr = NULL;
+		$Seeker = NULL;
+		$ReallyTho = FALSE;
 
-		$Before = $this->File->FindPrevious(
+		$Seeker = $Before = $this->File->FindPrevious(
 			[T_STATIC,T_PUBLIC,T_PROTECTED,T_PRIVATE,T_COMMA,T_CONST],
 			($StackPtr-1),
 			NULL
 		);
+
+		// make sure its not actually a typed property.
+
+		while($ReallyTho && $this->GetTypeFromStack($Seeker) !== T_SEMICOLON)
+		if($this->GetTypeFromStack($Seeker++) === T_CONST)
+		$ReallyTho = TRUE;
+
+		if(!$ReallyTho)
+		return;
 
 		if($this->GetTypeFromStack($this->StackPtr-1) === T_WHITESPACE) {
 			if($this->GetLineFromStack($Before) === $this->GetLineFromStack($this->StackPtr)) {
